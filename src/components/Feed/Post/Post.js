c8516507162cdef3ac4236dd870983aa9792db8a
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-26 21:16:50 +0300

    Send tweets to firebase

--- src/components/Feed/Post/Post.js
+++ src/components/Feed/Post/Post.js
@@ -36,7 +36,7 @@ const Post = ({
             <p>{text}</p>
           </div>
         </div>
-        <img src={image} alt="user img" />
+        <img src={image} alt="" />
         <div className="post__footer">
           <ChatBubbleOutlineIcon fontSize="small" />
           <RepeatIcon fontSize="small" />
--- src/components/Feed/TweetBox/TweetBox.js
+++ src/components/Feed/TweetBox/TweetBox.js
@@ -1,16 +1,65 @@
 import { Avatar, Button } from "@material-ui/core";
+import { useState } from "react";
+import db from "../../../firebase";
 import "./TweetBox.css";
 
 const TweetBox = () => {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Humphrey Mutuma",
+      username: "HumphreyMutuma7",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
   return (
     <div className="tweetBox">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="tweetBox__input">
           <Avatar src="https://cdn.pixabay.com/photo/2021/05/21/16/58/woman-6271553_960_720.jpg" />
-          <input placeholder="What's happening? " type="text" />
+          <input
+            value={tweetMessage}
+            onChange={(e) => setTweetMessage(e.target.value)}
+            placeholder="What's happening? "
+            type="text"
+          />
         </div>
-        <input className="tweetBox__imageInput" placeholder="Enter image URL " type="text" />
-        <Button className="tweetBox__tweetButton">Tweet</Button>
+        <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox__imageInput"
+          placeholder="Enter image URL "
+          type="text"
+        />
+        {tweetMessage ? (
+          <Button
+            onSubmit={handleSubmit}
+            type="submit"
+            className="tweetBox__tweetButton"
+          >
+            Tweet
+          </Button>
+        ) : (
+          <Button
+            onSubmit={handleSubmit}
+            type="submit"
+            className="tweetBox__tweetButtonDisabled"
+            disabled
+          >
+            Tweet
+          </Button>
+        )}
+        
       </form>
     </div>
   );
